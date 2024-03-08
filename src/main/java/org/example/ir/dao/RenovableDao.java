c1393ab474d0d19b4418b5523ae42ec15a735f82
Mohamed-bajadi <149510285+Mohamed-bajadi@users.noreply.github.com>
2024-03-08 16:01:01 +0100

    Update RenovableDao.java

--- src/main/java/org/example/ir/dao/RenovableDao.java
+++ src/main/java/org/example/ir/dao/RenovableDao.java
@@ -2,13 +2,12 @@ package org.example.ir.dao;
 
 import org.example.ir.bean.Renovable;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
+
 import org.springframework.stereotype.Repository;
 
 @Repository
-=======
 
->>>>>>> 7d8b19906d5bd1e4f147065c5c8313a7f5182e6c
+
 public interface RenovableDao extends JpaRepository<Renovable,Integer> {
     Renovable findRenovableById(Long id);
     int deleteRenovableById(Long id);
